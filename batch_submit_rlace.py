7e7014dc0558562cf01db652bf7c79e535fd57ae
Kevin Du <kevin.du20@gmail.com>
2023-03-31 01:45:34 +0200

    fix bug with f string for batch_submit_rlace.py

--- batch_submit_rlace.py
+++ batch_submit_rlace.py
@@ -5,4 +5,4 @@ for seed in seeds:
     for c in num_classes:
         for rank in range(1, c + 1):
             # print(c, rank, seed)
-            subprocess.check_call(["sbatch", "submit_rlace.cluster", f"{c}", f"{rank}", "{seed}"])
+            subprocess.check_call(["sbatch", "submit_rlace.cluster", f"{c}", f"{rank}", f"{seed}"])
